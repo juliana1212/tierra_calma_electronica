63d772d1102f239340d56bab682805578b49a4d8
abaunzaaa <adiaz662@soyudemedellin.edu.co>
2025-10-18 21:30:07 -0500

    Se añadió por completo la vista nueva Chatbot y se modificó el diseño de la vista monstera

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,19 +1,16 @@
 import { Routes } from '@angular/router';
 
-// Layouts
 import { PublicLayoutComponent } from './layouts/public-layout';
 import { PrivateLayoutComponent } from './layouts/private-layout';
 import { MonsteraComponent } from './pages/monstera/monstera';
-// Páginas públicas
 import { HomeComponent } from './pages/home/home';
 import { LoginComponent } from './pages/login/login';
+import { ChatbotComponent } from './pages/chatbot/chatbot';
 
-// Páginas privadas
 import { MisPlantasComponent } from './pages/mis-plantas/mis-plantas';
-import { RegistrarPlantasComponent } from './pages/registrar-plantas/registrar-plantas'; // ✅ agrega esto
+import { RegistrarPlantasComponent } from './pages/registrar-plantas/registrar-plantas';
 
 export const routes: Routes = [
-  // 🌿 Layout público (antes de iniciar sesión)
   {
     path: '',
     component: PublicLayoutComponent,
@@ -23,18 +20,20 @@ export const routes: Routes = [
     ]
   },
 
-  // 🌱 Layout privado (después del login)
   {
     path: '',
     component: PrivateLayoutComponent,
     children: [
       { path: 'mis-plantas', component: MisPlantasComponent },
       { path: 'registrar-plantas', component: RegistrarPlantasComponent },
-      { path: 'monstera', component: MonsteraComponent } // 👈 NUEVA RUTA
+      { path: 'monstera', component: MonsteraComponent }
     ]
   },
 
-  // Redirección por defecto
+  {path: 'chatbot', 
+  component: ChatbotComponent 
+  },
+
   { path: '**', redirectTo: '' }
 ];
 
--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',          // Etiqueta HTML personalizada
+  selector: 'app-home',
   imports: [],
-  templateUrl: './home.html',  // Archivo HTML
-  styleUrls: ['./home.scss']   // Archivo SCSS
+  templateUrl: './home.html',
+  styleUrls: ['./home.scss']
 })
 export class HomeComponent {
-  // Aquí va tu JavaScript - variables, funciones, etc.
   title = 'Bienvenida a Tierra en calma';
-  
-  // Ejemplo de función (como en JS)
   saludar() {
     alert('¡Hola desde TierraEnCalma!');
   }
